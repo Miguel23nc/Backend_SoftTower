c7dc75217b3393363f3769e6a1610b1ef2abb9ee
Miguel23nc <1392023@senati.pe>
2025-02-28 09:33:51 -0500

    patch

--- src/app.js
+++ src/app.js
@@ -26,7 +26,7 @@ app.use((req, res, next) => {
     res.header("Access-Control-Allow-Origin", "*");
   }
 
-  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS, PATCH");
   res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
 
   // Manejar preflight OPTIONS request
