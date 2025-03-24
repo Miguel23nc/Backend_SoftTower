e9e49746e90054c44eb425db5fbde6b3250c5456
Miguel23nc <1392023@senati.pe>
2025-03-24 09:27:40 -0500

    cors patch

--- src/app.js
+++ src/app.js
@@ -21,7 +21,7 @@ app.use((req, res, next) => {
 
   if (allowedOrigins.includes(origin)) {
     res.setHeader("Access-Control-Allow-Origin", origin);
-    res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+    res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, PATCH, DELETE");
     res.setHeader(
       "Access-Control-Allow-Headers",
       "Content-Type, Authorization"
