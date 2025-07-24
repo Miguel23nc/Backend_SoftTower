19be985b3508760698f0724ce65ab4a2d882ec91
Miguel23nc <1392023@senati.pe>
2025-07-24 12:57:49 -0500

    paginacionAndSearchAsistencia

--- src/controllers/AllModulos/RecursosHumanos/Asistencia/colaborador/getAsistenciaByParams.js
+++ src/controllers/AllModulos/RecursosHumanos/Asistencia/colaborador/getAsistenciaByParams.js
@@ -1,20 +1,45 @@
+const Employee = require("../../../../../models/Employees/Employee");
 const AsistenciaColaborador = require("../../../../../models/RecursosHumanos/AsistenciaColaborador");
-
+function escapeRegExp(string) {
+  return string.replace(/[.*+?^${}()|[\]\\]/g, "\\$&");
+}
 const getAsistenciaByParams = async (req, res) => {
+  const { page = 0, limit = 10, search = "" } = req.query;
   try {
-    const { page = 0, limit = 10 } = req.query;
+    const query = {};
+    if (search) {
+      const safeSearch = escapeRegExp(search);
+      const regex = new RegExp(safeSearch, "i");
+      const colaboradores = await Employee.find({
+        $or: [
+          { name: regex },
+          { lastname: regex },
+          { business: regex },
+          { charge: regex },
+        ],
+      }).select("_id");
+
+      const colaboradoresIds = colaboradores.map((c) => c._id);
+      query.$or = [
+        { colaborador: { $in: colaboradoresIds } },
+        { state: regex },
+      ];
+    }
 
     const [data, total] = await Promise.all([
-      AsistenciaColaborador.find()
+      AsistenciaColaborador.find(query)
         .skip(page * limit)
         .limit(parseInt(limit))
         .sort({ createdAt: -1 })
-        .populate("colaborador", "name lastname "),
-      AsistenciaColaborador.countDocuments(),
+        .populate("colaborador", "name lastname ")
+        .lean(),
+      AsistenciaColaborador.countDocuments(query),
     ]);
 
     return res.json({ data, total });
   } catch (err) {
+    console.log("Error al obtener las asistencias:", err);
+    
     return res
       .status(500)
       .json({ message: err.message || "Error al buscar las Asistencias" });
